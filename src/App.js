3746ef8fe6c7d0f6815aed90affea60c521594bc
Souvik Konar <sk5305@srmist.edu.in>
2026-06-23 21:17:28 +0530

    Fix title encoding issue

--- src/App.js
+++ src/App.js
@@ -1,14 +1,15 @@
-// src/App.js
+﻿// src/App.js
 import React from 'react';
 import CodeEditor from './components/CodeEditor';
 
 function App() {
   return (
     <div className="App">
-      <h1>🧠 IntelliCode: AI Coding Assistant</h1>
+      <h1>IntelliCode: AI Coding Assistant</h1>
       <CodeEditor />
     </div>
   );
 }
 
 export default App;
+
